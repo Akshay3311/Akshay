128c223fb825ff32889aee4298b17bedcf83604d
Akshay3311 <akshaykharpas111@gmail.com>
2024-12-30 13:26:42 +0530

    Update Ak.java

--- Ak.java
+++ Ak.java
@@ -6,6 +6,7 @@ public class Ak{
         System.out.println("Sum of two 5 7 10 is:" + (a+b));
         System.out.println("Division is");
         System.out.println("Changesp are save");
+        System.out.println("hlo sir");
         c = a=b;
     }
 }
