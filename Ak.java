c67af2503d18e71d58be661807bdf58e1ee27075
SR4Xlucifer <sr4sohamwadale@gmail.com>
2024-12-30 12:01:34 +0530

    Omsairam

---
+++ Ak.java
@@ -0,0 +1,9 @@
+public class Ak{
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c;
+        System.out.println("Sum of two 5 7 10 is:" + (a+b));
+        c = a=b;
+    }
+}
